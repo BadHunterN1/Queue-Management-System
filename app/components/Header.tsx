ef058c442f070ce77a4c23516621c481fed40cb2
BadHunterN1 <ahmam2004@gmail.com>
2025-07-05 21:28:39 +0300

    ...

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
+
 export default function Header() {
 	return (
 		<header className="absolute top-0 z-20 w-full">
-			<div className="text-3xl font-bold text-blue-600 w-full flex items-center justify-center md:px-6 drop-shadow-blue-600">
+			<Link
+				href={"/"}
+				className="text-3xl font-bold text-blue-600 w-full flex items-center justify-center md:px-6 drop-shadow-blue-600">
 				ريماس
-			</div>
+			</Link>
 		</header>
 	);
 }
